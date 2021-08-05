6a92350163fd583caa5119bd77f8f7e19b81745b
HakureiHikaze <wgyaaa123@gmail.com>
2021-08-05 15:04:01 +0800

    create files

---
+++ SinglyLinkedList.h
@@ -0,0 +1,11 @@
+/**
+ * @file SinglyLinkedList.h
+ * @author Hikaze (wgyaaa123@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<iostream>
---
+++ experiment.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file experiment.cpp
+ * @author Hikaze (wgyaaa123@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<iostream>
+#include<stdlib.h>
+
+#include "SinglyLinkedList.h"
+
+int main(int argc, char** argv){
+    std::cout<<"Helloworld"<<std::endl;
+    system("pause");
+    exit(0);
+}
